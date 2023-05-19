9795e1892ae14bf10403d9dc57339f673af436e0
Zach Green <greenzary@gmail.com>
2023-05-19 15:38:59 -0500

    added commentDAO

--- app/src/main/java/postbored/dynamodb/CommentDao.java
+++ app/src/main/java/postbored/dynamodb/CommentDao.java
@@ -3,19 +3,20 @@ package postbored.dynamodb;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import postbored.dynamodb.models.Comment;
 import postbored.dynamodb.models.Post;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.inject.Inject;
-import javax.inject.Singleton;
 
 /**
  * Accesses data for a playlist using {@link Post} to represent the model in DynamoDB.
  */
 @Singleton
-public class PostDao {
+public class CommentDao {
     private final DynamoDBMapper dynamoDbMapper;
 
     /**
@@ -25,21 +26,21 @@ public class PostDao {
      *
      */
     @Inject
-    public PostDao(DynamoDBMapper dynamoDbMapper) {
+    public CommentDao(DynamoDBMapper dynamoDbMapper) {
         this.dynamoDbMapper = dynamoDbMapper;
     }
 
     /**
-     * Returns the {@link Post} corresponding to the specified id.
+     * Returns the {@link Comment} corresponding to the specified id.
      *
-     * @param id the Message ID
-     * @return the stored Playlist, or null if none was found.
+     * @param id the Comment ID
+     * @return the stored Comment, or null if none was found.
      */
-    public Post getPost(String id) {
-        Post post = this.dynamoDbMapper.load(Post.class, id);
+    public Comment getComment(String id) {
+        Comment post = this.dynamoDbMapper.load(Comment.class, id);
 
         if (post == null) {
-            throw new IllegalArgumentException("Could not find message with id " + id);
+            throw new IllegalArgumentException("Could not find comment with id " + id);
         }
 
         return post;
@@ -48,12 +49,12 @@ public class PostDao {
     /**
      * Saves (creates or updates) the given playlist.
      *
-     * @param post The playlist to save
-     * @return The Playlist object that was saved
+     * @param comment The comment to save
+     * @return The Comment object that was saved
      */
-    public Post savePost(Post post) {
-        this.dynamoDbMapper.save(post);
-        return post;
+    public Comment saveComment(Comment comment) {
+        this.dynamoDbMapper.save(comment);
+        return comment;
     }
 
     /**
@@ -67,7 +68,7 @@ public class PostDao {
      * @param criteria an array of String containing search criteria.
      * @return a List of Playlist objects that match the search criteria.
      */
-    public List<Post> searchPosts(String[] criteria) {
+    public List<Comment> searchPosts(String[] criteria) {
         DynamoDBScanExpression dynamoDBScanExpression = new DynamoDBScanExpression();
 
         if (criteria.length > 0) {
@@ -81,7 +82,7 @@ public class PostDao {
                 valueMap.put(valueMapNamePrefix + i,
                         new AttributeValue().withS(criteria[i]));
                 nameFilterExpression.append(
-                        filterExpressionPart("postID", valueMapNamePrefix, i));
+                        filterExpressionPart("commentID", valueMapNamePrefix, i));
                 tagsFilterExpression.append(
                         filterExpressionPart("timeSent", valueMapNamePrefix, i));
             }
@@ -91,7 +92,7 @@ public class PostDao {
                     "(" + nameFilterExpression + ") or (" + tagsFilterExpression + ")");
         }
 
-        return this.dynamoDbMapper.scan(Post.class, dynamoDBScanExpression);
+        return this.dynamoDbMapper.scan(Comment.class, dynamoDBScanExpression);
     }
 
     private StringBuilder filterExpressionPart(String target, String valueMapNamePrefix, int position) {
