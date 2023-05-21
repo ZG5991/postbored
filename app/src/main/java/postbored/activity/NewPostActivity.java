fa1491ff17ac2dbd92a635ddad103d7f32349e07
Zach Green <greenzary@gmail.com>
2023-05-21 16:45:11 -0500

    fleshed out NewPostActivity class, should be Lambda-ready

--- app/src/main/java/postbored/activity/NewPostActivity.java
+++ app/src/main/java/postbored/activity/NewPostActivity.java
@@ -1,5 +1,6 @@
 package postbored.activity;
 
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import postbored.activity.requests.NewPostRequest;
 import postbored.activity.results.NewPostResult;
 import postbored.dynamodb.PostDao;
@@ -8,10 +9,14 @@ import postbored.models.PostModel;
 import postbored.utilities.ModelConverter;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class NewPostActivity {
 
     private final PostDao postDao;
+    private final ObjectIdGenerators.UUIDGenerator generator = new ObjectIdGenerators.UUIDGenerator();
 
     @Inject
     public NewPostActivity(PostDao postDao) {
@@ -21,17 +26,23 @@ public class NewPostActivity {
     public NewPostResult handleRequest(final NewPostRequest newPostRequest) {
 
         //TODO : add logic here to handle null/empty stuff!!
+        List<String> postComments;
+        if (newPostRequest.getComments() != null && newPostRequest.getComments().isEmpty()) {
+            postComments = new ArrayList<>();
+        } else postComments = newPostRequest.getComments();
 
+        String uniqueID = UUID.randomUUID().toString();
         Post post = new Post();
-        post.setPostID("PLACEHOLDER"); //generate this as a new UUID
+
+        post.setPostID(uniqueID);
         post.setDateSent(newPostRequest.getTimeSent());
         post.setPostTitle(newPostRequest.getPostTitle());
         post.setPostBody(newPostRequest.getPostBody());
         post.setPosterID(newPostRequest.getPosterID());
         post.setPosterName(newPostRequest.getPosterName());
         post.setTopic(newPostRequest.getTopic());
-        post.setComments(newPostRequest.getComments()); //initialize an empty list if null
-        post.setLikesCounter(newPostRequest.getLikesCounter()); //should default to zero
+        post.setComments(postComments);
+        post.setLikesCounter(0); //should always default to zero for a new post
 
         postDao.savePost(post);
 
